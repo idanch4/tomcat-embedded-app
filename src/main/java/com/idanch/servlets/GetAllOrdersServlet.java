75a82768354864eb62965687bc10de5c58a2c8cf
idanch4 <idanch4@gmail.com>
2020-07-11 14:45:10 +0300

    added all order info to /listOrders endpoint

--- src/main/java/com/idanch/servlets/GetAllOrdersServlet.java
+++ src/main/java/com/idanch/servlets/GetAllOrdersServlet.java
@@ -1,8 +1,12 @@
 package com.idanch.servlets;
 
+import com.idanch.data.factories.MenuDaoFactory;
 import com.idanch.data.factories.OrdersDaoFactory;
+import com.idanch.data.interfaces.MenuDao;
 import com.idanch.data.interfaces.OrdersDao;
+import com.idanch.data.representations.Dish;
 import com.idanch.data.representations.RestaurantOrder;
+import com.idanch.data.util.OrdersUtil;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -13,15 +17,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.Map;
 
 @WebServlet("/getAllOrders")
 public class GetAllOrdersServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         OrdersDao ordersDao = OrdersDaoFactory.getOrdersDao();
+        MenuDao menuDao = MenuDaoFactory.getMenuDao();
+
         List<RestaurantOrder> orders = ordersDao.getAllOrders();
+        List<Dish> menuItems = menuDao.getAllDishes();
 
         JSONArray ordersJSONArr = new JSONArray();
+        JSONArray contentsJSONArr;
         JSONObject orderJSON;
         for (RestaurantOrder order: orders) {
             orderJSON = new JSONObject();
@@ -29,6 +38,31 @@ public class GetAllOrdersServlet extends HttpServlet {
             orderJSON.put("id", order.getId());
             orderJSON.put("customer", order.getCustomer());
             orderJSON.put("status", order.getStatus());
+
+            JSONObject menuItemJSON;
+            contentsJSONArr = new JSONArray();
+            Map<Long, Integer> contents = order.getContents();
+
+            // insert all items on the order into the JSON
+            int index = 0;
+            for (Dish dish: menuItems) {
+                if (contents.containsKey(dish.getId())) {
+                    menuItemJSON = new JSONObject();
+
+                    menuItemJSON.put("name", dish.getName());
+                    menuItemJSON.put("category", dish.getCategory());
+                    menuItemJSON.put("description", dish.getDescription());
+                    menuItemJSON.put("priceShekels", dish.getPriceShekels());
+
+                    contentsJSONArr.put(index, menuItemJSON);
+                    index++;
+                }
+            }
+
+            orderJSON.put("contents", contentsJSONArr);
+
+            Double totalPrice = ordersDao.calculateTotal(order.getId());
+            orderJSON.put("totalPrice", totalPrice);
 
             ordersJSONArr.put(orders.indexOf(order), orderJSON);
         }
--- src/main/webapp/js/listOrders.js
+++ src/main/webapp/js/listOrders.js
@@ -14,14 +14,32 @@ function getAllOrders() {
 
 getAllOrders();
 
+
+// long-poll the server for any new orders and update the DOM immediately
+function checkForNewOrders() {
+
+}
+
 // constructs and adds a div for an @order object
 // @order {id: long, customer: string, status: string, contents: @Dictionary}
 function addOrder(order) {
   var contents = document.getElementById("contents");
   var orderDiv = document.createElement("div");
   orderDiv.className = "order";
+  orderDiv.setAttribute("id", "order_" + order.id);
   orderDiv.innerHTML = "<h3>Order #" + order.id + " by " +
-   order.customer + "</h3>" +
-    "<p>Total: " + order.totalPrice + " NIS</p>";
+   order.customer + "</h3>";
+
+  for(var i = 0; i < order.contents.length; i++) {
+    orderDiv.innerHTML += "<p>" + order.contents[i].name + " - " +
+    order.contents[i].priceShekels + " NIS</p>";
+  }
+
+  orderDiv.innerHTML += "<p>Total: " + order.totalPrice + " NIS</p>";
   contents.appendChild(orderDiv);
+}
+
+function removeOrder(id) {
+  var orderEl = document.getElementById("order_" + id);
+  document.getElementById("contents").removeChild(orderEl);
 }
